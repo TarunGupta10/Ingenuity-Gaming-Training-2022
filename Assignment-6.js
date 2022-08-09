a1c4b68688d0b45d6acf85bfb13749941a1b1cf7
TarunGupta10 <tarunhl1999@gmail.com>
2022-08-09 15:09:37 +0530

    commit

--- Assignment-6.js
+++ Assignment-6.js
@@ -6,8 +6,19 @@ var date2 = "Today Day : " + weekday[today.getDay()];
 document.write(date1 );
 document.write(  "<br>" + date2);
 
+
+// Ques = 2
+
+document.body.style.backgroundColor="burlywood";
+document.body.style.fontSize="25px";
+document.body.style.color="green";
+
 var name = window.prompt("Enter your name: ");
 document.write("<br>" + "Your name is " + name);
+
+
+
+// Ques = 3
 
 var first = parseInt(window.prompt("Enter first num"));
 var second = parseInt(window.prompt("Enter second num"));
@@ -16,7 +27,10 @@ var Subtraction = first - second;
 var Multiplication = first * second;
 var Division = first / second;
 
-document.write("<br>" + "<i> Addition is -> </i>" + Addition);
-document.write("<br>" + "<i>  Subtraction is -> </i>" + Subtraction);
-document.write("<br>" + "<i> Multiplication is -> </i>" + Multiplication);
-document.write("<br>" + "<i> Division is -> </i>" + Division);
+document.write("<br>" + "<i> Addition is = </i>" + Addition);
+document.write("<br>" + "<i>  Subtraction is = </i>" + Subtraction);
+document.write("<br>" + "<i> Multiplication is = </i>" + Multiplication);
+document.write("<br>" + "<i> Division is = </i>" + Division);
+
+// Ques = 4
+// node Assignment-6.js

34fe6253b1a836062d723f48edafbec4d8135434
TarunGupta10 <tarunhl1999@gmail.com>
2022-08-16 09:58:39 +0530

    comit

--- Day9 Functions/Function1.js
+++ Day9 Functions/Function1.js
@@ -1,2 +1,15 @@
 // function parts
 
+function myFunction1(){
+    console.log("This is function")
+    console.log("It will perform some task")
+
+}
+myFunction1();
+myFunction1();
+myFunction1();
+myFunction1();
+myFunction1();
+
+
+// 2. Function with arguments without return

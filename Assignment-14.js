d0883656ba13bda273d9bf93a9cab445cfba3aeb
TarunGupta10 <tarunhl1999@gmail.com>
2022-08-25 17:27:36 +0530

    commit

--- Assignment-14.js
+++ Assignment-14.js
@@ -1,3 +1,4 @@
+var cty = document.getElementById("cty")
 const API_URL = "https://api.covid19api.com/summary";
 async function getApiData(url) {
   console.log("You are Good to Go");
@@ -6,20 +7,33 @@ async function getApiData(url) {
   // console.log(result);
   displayData(result);
 }
-var i = 77;
 getApiData(API_URL);
 function displayData(serverData) {
   console.log(serverData);
-  var cty = document.getElementById("cty");
-  var dt = document.getElementById("dt");
 
-  cty.innerText = "" + serverData.Countries[i].Country;
-  var today = new Date(serverData.Countries[i].Date);
-  var date1 =
-    today.getDate() + "/" + (today.getMonth() + 1) + "/" + today.getFullYear();
-  dt.innerText = "" + date1;
-  document.getElementById("tc").innerText = "" + serverData.Countries[i].TotalConfirmed;
-  document.getElementById("td").innerText = "" + serverData.Countries[i].TotalDeaths;
-  document.getElementById("nc").innerText = "" + serverData.Countries[i].NewConfirmed;
-  document.getElementById("nd").innerText = "" + serverData.Countries[i].NewDeaths;
-}
+  for (let i=2;i<190;i++) {
+    let option = document.createElement("option");
+    option.value = i
+    option.innerHTML  = serverData.Countries[i].Country
+    cty.appendChild(option);
+  }
+
+  function onChanges(){
+    var value = cty.value
+    var dt = document.getElementById("dt");
+    var today = new Date(serverData.Countries[value].Date);
+    var date1 =
+      today.getDate() + "/" + (today.getMonth() + 1) + "/" + today.getFullYear();
+    dt.innerText = "" + date1;
+    document.getElementById("tc").innerText = "" + serverData.Countries[value].TotalConfirmed;
+    document.getElementById("td").innerText = "" + serverData.Countries[value].TotalDeaths;
+    document.getElementById("nc").innerText = "" + serverData.Countries[value].NewConfirmed;
+    document.getElementById("nd").innerText = "" + serverData.Countries[value].NewDeaths;
+  }
+  cty.onchange = onChanges;
+  onChanges();
+  }
+
+
+  
+  
